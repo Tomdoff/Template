1ed613f1b880e18d352790e81466564a65424649
Tom Doyle <tom.doyle@method4.co.uk>
2019-04-30 12:54:33 +0100

    added unit test for controller

--- Supermarket.UnitTests/CategoriesControllerTests.cs
+++ Supermarket.UnitTests/CategoriesControllerTests.cs
@@ -1,8 +1,14 @@
+using AutoMapper;
 using Moq;
+using Supermarket.API.Controllers;
+using Supermarket.API.Mapping;
+using Supermarket.API.Resources;
 using Supermarket.Domain.Models;
 using Supermarket.Domain.Services;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 
 namespace Supermarket.UnitTests
@@ -10,17 +16,46 @@ namespace Supermarket.UnitTests
     public class CategoriesControllerTests
     {
         [Fact]
-        public async Task  GetAllAsync_ReturnsAListOfAllCategories()
+        public async Task GetAllAsync_ReturnsAListOfAllCategories()
         {
-            var mockService = new Mock<ICategoryService>();
-            mockService.Setup(repo => repo.ListAsync())
-	            .ReturnsAsync(GetTestCategories());
-            //var controller = new CategoriesController(mockService.Object, )
-            //Assert.NotNull(mockRepo);
+			//set up
+	        var controller = GetController();
+            var expectedResult = GetTestCategoryResources();
+
+            var result = await controller.GetAllAsync();
+            var actualResult = result.ToList();
+            expectedResult.Should().BeEquivalentTo(actualResult);
         }
 
-        private IEnumerable<Category> GetTestCategories() {
-	        yield break;
+        private List<Category> GetTestCategories()
+        {
+            List<Category> result = new List<Category> {
+                new Category { Id = 100, Name = "Fruits and Vegetables" }, // Id set manually due to in-memory provider
+		        new Category { Id = 101, Name = "Dairy" }
+            };
+
+            return result;
+        }
+
+        private List<CategoryResource> GetTestCategoryResources()
+        {
+            List<CategoryResource> result = new List<CategoryResource> {
+                new CategoryResource { Id = 100, Name = "Fruits and Vegetables" }, // Id set manually due to in-memory provider
+		        new CategoryResource { Id = 101, Name = "Dairy" }
+            };
+
+            return result;
+        }
+
+        private CategoriesController GetController()
+        {
+            var mockService = new Mock<ICategoryService>();
+            mockService.Setup(repo => repo.ListAsync()).ReturnsAsync(GetTestCategories());
+            var profile = new ModelToResourceProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(profile));
+            var mapper = new Mapper(configuration);
+            var controller = new CategoriesController(mockService.Object, mapper);
+            return controller;
         }
     }
 }
