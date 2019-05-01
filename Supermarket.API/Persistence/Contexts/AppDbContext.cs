3896dc2d987e9da8cd7e9617eec36d0fc15543bc
Brady Morgan <brady.morgan@method4.co.uk>
2019-05-01 13:08:15 +0100

    Fluent API update.
    Removed assembly added NuGet package.

--- Supermarket.API/Persistence/Contexts/AppDbContext.cs
+++ Supermarket.API/Persistence/Contexts/AppDbContext.cs
@@ -1,59 +1,59 @@
 using Microsoft.EntityFrameworkCore;
 using Supermarket.API.Domain.Models;
 
-namespace Supermarket.API.Persistence.Contexts
-{
-    public class AppDbContext : DbContext
-    {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
+namespace Supermarket.API.Persistence.Contexts {
+	public class AppDbContext : DbContext {
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
 
-        }
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+		}
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+		protected override void OnModelCreating(ModelBuilder builder) {
+			base.OnModelCreating(builder);
 
-            builder.Entity<Category>().ToTable("Categories");
-            builder.Entity<Category>().HasKey(p => p.Id);
-            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
-            // a category has many products. each product has only 1 category, with categoryId as the foreign key.
-            builder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
+			builder.Entity<Category>(e => {
+				e.HasKey(p => p.Id);
+				e.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+				e.Property(p => p.Name).IsRequired().HasMaxLength(30);
 
-            //has data allows us to configure seed data
-            builder.Entity<Category>().HasData(
-                new Category { Id = 100, Name = "Fruits and Vegetables" }, // Id set manually due to in-memory provider
-                new Category { Id = 101, Name = "Dairy" }
-            );
+				//A category has many products. each product has only 1 category, with categoryId as the foreign key.
+				e.HasMany(p => p.Products)
+					.WithOne(p => p.Category)
+					.HasForeignKey(p => p.CategoryId);
 
-            builder.Entity<Product>().ToTable("Products");
-            builder.Entity<Product>().HasKey(p => p.Id);
-            builder.Entity<Product>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Entity<Product>().Property(p => p.QuantityInPackage).IsRequired();
-            builder.Entity<Product>().Property(p => p.UnitOfMeasurement).IsRequired();
+				//has data allows us to configure seed data
+                e.HasData(
+					new Category { Id = 100, Name = "Fruits and Vegetables" }, // Id set manually due to in-memory provider
+					new Category { Id = 101, Name = "Dairy" }
+				);
+            });
 
-            builder.Entity<Product>().HasData(
-                new Product
-                {
-                    Id = 100,
-                    Name = "Apple",
-                    QuantityInPackage = 1,
-                    UnitOfMeasurement = EUnitOfMeasurement.Unity,
-                    CategoryId = 100
-                },
-                new Product
-                {
-                    Id = 101,
-                    Name = "Milk",
-                    QuantityInPackage = 2,
-                    UnitOfMeasurement = EUnitOfMeasurement.Liter,
-                    CategoryId = 101,
-                }
-            );
-        }
-    }
+
+			builder.Entity<Product>(e => {
+				e.HasKey(p => p.Id);
+				e.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+				e.Property(p => p.Name).IsRequired().HasMaxLength(50);
+				e.Property(p => p.QuantityInPackage).IsRequired();
+				e.Property(p => p.UnitOfMeasurement).IsRequired();
+
+				e.HasData(
+					new Product {
+						Id = 100,
+						Name = "Apple",
+						QuantityInPackage = 1,
+						UnitOfMeasurement = EUnitOfMeasurement.Unity,
+						CategoryId = 100
+					},
+					new Product {
+						Id = 101,
+						Name = "Milk",
+						QuantityInPackage = 2,
+						UnitOfMeasurement = EUnitOfMeasurement.Liter,
+						CategoryId = 101,
+					}
+				);
+            });
+		}
+	}
 }
